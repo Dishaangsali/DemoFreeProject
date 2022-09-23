393015f27d605975898a6996f1cb0519ee0b0f93
Dishaangsali <98702368+Dishaangsali@users.noreply.github.com>
2022-09-23 10:34:16 +0530

    Create InputName.java

---
+++ InputName.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class InputName {
+  public static void main(String args[]) {
+    System.out.println("Enter your name: ");
+    String name=sc.nextLine();
+    System.out.println("Welcome" +name+);
+  }
+}
